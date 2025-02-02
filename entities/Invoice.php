adb00bc33fd4d5c999396f16fc7900936b0798ee
Win <nguyennrdz@gmail.com>
2025-02-02 17:45:25 +0700

    2/2/2025/5h

--- entities/Invoice.php
+++ entities/Invoice.php
@@ -7,15 +7,21 @@ class Invoice {
     public $totalAmount;
     public $status;
     public $paymentType;
+    public $NumberPhone;
+
+    public $Address;
 
     // Constructor
-    public function __construct($invoiceID, $userID, $invoiceDate, $totalAmount, $status, $paymentType) {
+    public function __construct($invoiceID, $userID, $invoiceDate,
+     $totalAmount, $status, $paymentType,$NumberPhone,$Address) {
         $this->invoiceID = $invoiceID;
         $this->userID = $userID;
         $this->invoiceDate = $invoiceDate;
         $this->totalAmount = $totalAmount;
         $this->status = $status;
         $this->paymentType = $paymentType;
+        $this->NumberPhone = $NumberPhone;
+        $this->Address = $Address;
     }
 
 }
