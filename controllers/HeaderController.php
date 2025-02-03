e948a58813c392269178b686119453297cc7d078
Win <nguyennrdz@gmail.com>
2025-02-03 21:11:11 +0700

    end search

--- controllers/HeaderController.php
+++ controllers/HeaderController.php
@@ -3,14 +3,17 @@ class HeaderController extends BaseController
 {
 
     private $UserModel;
+    private $ProductModel;
     public function __construct()
     {
         $this->UserModel = $this->loadModel("UserModel");
+        $this->ProductModel = $this->loadModel("ProductModel");
     }
 
     public function index() {
         $accountID = $this->takeIDAccount();
         $dataUser = $this->UserModel->getUserByID($accountID);
+        $_SESSION['Product'] = json_encode($this->ProductModel->getAllProduct());
         if ($dataUser) {
             $data = [
                 'username' => $dataUser->FullName,
---
+++ controllers/SearchController.php
@@ -0,0 +1,54 @@
+<?php
+class SearchController extends BaseController
+{
+
+    private $ProductModel;
+
+    public function __construct()
+    {
+       
+        $this->ProductModel = $this->loadModel("ProductModel");
+    }
+
+    public function index() {
+    
+        $this->view('frontEnd.search.index');
+    }
+    public function searchProduct($string) {
+        $data = $this->getAllProduct();
+        
+        $productDataSearch = [];
+        foreach ($data as $items) {
+            if (stripos($items->productName, $string) !== false) {
+                $productDataSearch[] = $items;
+            }
+        }
+        if (count($productDataSearch) == 0) {
+            $_SESSION['error'] = "There are no products found.";
+        }
+        $this->view('frontEnd.search.index',['data'=>$productDataSearch]);
+    }
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $action = $_POST['action'] ?? null;
+
+    switch ($action) {
+        case 'search':
+            $searchController = new SearchController();
+            $string = $_POST['string'] ?? null;
+            if ($string) {
+               
+                $searchController->searchProduct($string);
+                exit;
+            } else {
+                $_SESSION['error'] = "There are no products found.";
+                $searchController->index();
+                exit;
+            }
+            break;
+
+        default:
+            echo "Hành động không hợp lệ!";
+            break;
+    }
+}
