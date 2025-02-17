7e9d37cb594f8c2f2f6d5966b91688a43857e0be
T14 Gen 3 <nguyennrdz@gmail.com>
2025-02-17 22:32:37 +0700

    add search

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -15,10 +15,41 @@ class SearchController extends BaseController
         $dataLineProduct=$this->ProductModel->getLineProduct();
         $this->view('frontEnd.search.index',['dataLineProduct'=>$dataLineProduct]);
     }
+    public function CleanAll() {   
+        $_SESSION['ProductLineSearch'] = '';
+        $_SESSION['From']='';
+        $_SESSION['To']='';
+        $dataLineProduct=$this->ProductModel->getLineProduct();
+        $this->view('frontEnd.search.index',['dataLineProduct'=>$dataLineProduct]);
+    }
+
     public function searchProduct($string) {
         $data = $this->getAllProduct();
         $dataLineProduct=$this->ProductModel->getLineProduct();
         $productDataSearch = [];
+        $productLineName='';
+        $ProductLineAdd = isset($_SESSION['ProductLineSearch']) ? $_SESSION['ProductLineSearch'] : ' ';
+        if($ProductLineAdd!==''){
+            $productLineName=$this->ProductModel-> getnameLine( $ProductLineAdd);
+        }
+        $FromAdd = isset($_SESSION['From']) ? $_SESSION['From'] : ' ';
+        $ToAdd = isset($_SESSION['To']) ? $_SESSION['To'] : ' ';
+        if( $ProductLineAdd!=''&& $FromAdd!=''&& $ToAdd!=''){
+            foreach ($data as $items) {
+                if (stripos($items->productName, $string) !== false&&$items->productLineID ==  $ProductLineAdd && $items->price >= $FromAdd && $items->price <= $ToAdd) {
+                    $productDataSearch[] = $items;
+                }
+            }
+        }
+        else
+        if( $ProductLineAdd==''&& $FromAdd!=''&& $ToAdd!=''){
+            foreach ($data as $items) {
+                if (stripos($items->productName, $string) !== false&& $items->price >= $FromAdd && $items->price <= $ToAdd) {
+                    $productDataSearch[] = $items;
+                }
+            }
+        }
+        else
         foreach ($data as $items) {
             if (stripos($items->productName, $string) !== false) {
                 $productDataSearch[] = $items;
@@ -27,30 +58,53 @@ class SearchController extends BaseController
         if (count($productDataSearch) == 0) {
             $_SESSION['error'] = "There are no products found.";
         }
-        $this->view('frontEnd.search.index',['dataPrd'=>$productDataSearch,'dataLineProduct'=>$dataLineProduct]);
+        $this->view('frontEnd.search.index',
+        ['dataPrd'=>$productDataSearch,
+        'dataLineProduct'=>$dataLineProduct,
+        'productLineName'=> $productLineName,
+        'FromAdd'=>$FromAdd,
+        'ToAdd'=>$ToAdd]);
     }
 
-    public function searchWithConditions($ProductLines,$From,$To) {
-
+    public function searchWithConditions($ProductLines, $From, $To) {
         $data = $this->getAllProduct();
-        $dataLineProduct=$this->ProductModel->getLineProduct();
+        $dataLineProduct = $this->ProductModel->getLineProduct();
         $productDataSearch = [];
+        $productLineName='';
+        $ProductLineAdd = isset($_SESSION['ProductLineSearch']) ? $_SESSION['ProductLineSearch'] : ' ';
+        if($ProductLineAdd!==''){
+            $productLineName=$this->ProductModel-> getnameLine( $ProductLineAdd);
+        }
+        $FromAdd = isset($_SESSION['From']) ? $_SESSION['From'] : ' ';
+        $ToAdd = isset($_SESSION['To']) ? $_SESSION['To'] : ' ';
+
+        
+     
         foreach ($data as $items) {
-            if ($items->productLineID == $ProductLines&& $items->price<=$To && $items->price>=$To ) {
+            if ($ProductLines == '') {
+                if ($items->price >= $From && $items->price <= $To) {
+                    $productDataSearch[] = $items;
+                }
+               
+            } else
+            if ($items->productLineID == $ProductLines && $items->price >= $From && $items->price <= $To) {
                 $productDataSearch[] = $items;
             }
         }
+
         if (count($productDataSearch) == 0) {
             $_SESSION['error'] = "There are no products found.";
         }
-        $this->view('frontEnd.search.index',['dataPrd'=>$productDataSearch,'dataLineProduct'=>$dataLineProduct]);
- 
+        $this->view('frontEnd.search.index', [
+            'dataPrd' => $productDataSearch,
+            'dataLineProduct' => $dataLineProduct,
+            'productLineName'=> $productLineName,
+            'FromAdd'=>$FromAdd,
+            'ToAdd'=>$ToAdd
 
-
-
-
-
+        ]);
     }
+    
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $action = $_POST['action'] ?? null;
@@ -69,12 +123,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 exit;
             }
         case 'searchWithConditions':
-            $ProductLine=$_POST['ProductLine'];
+            if (!isset($ProductLine)) {
+                $ProductLine = isset($_POST['ProductLine']) ? $_POST['ProductLine'] : '';
+            }           
+         
+            $_SESSION['ProductLineSearch'] = $ProductLine;
             $From=$_POST['From'];
+            $_SESSION['From']=$From;
             $To=$_POST['To'];
+            $_SESSION['To']=$To;
             $searchController->searchWithConditions($ProductLine,$From,$To);
-
-
             exit();
 
 
