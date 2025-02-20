bc570dce4c302c20704f89bc4cdbab7ec84266c7
T14 Gen 3 <nguyennrdz@gmail.com>
2025-02-20 23:11:57 +0700

    3/20/2

--- controllers/AddquantityController.php
+++ controllers/AddquantityController.php
@@ -3,36 +3,24 @@ require_once __DIR__ ."/../entities/Product.php";
 
 class AddquantityController extends BaseController
 {
-
-
     private $ProductModel;
-    
-
     public function __construct()
     {
         $this->ProductModel = $this->loadModel("ProductModel");
-      
     }
     public function index()
     {
         $id=$_GET['id'];
         $ProductEdit = $this->ProductModel->getProductByID($id);
-       
         $this->view('manager.AddQuantity.index',['ProductEdit'=>$ProductEdit]);
     }
-   
-   
-  
+    // thêm số lượng cho sản phẩm
     public function AddQuantity($id,$stock) {
-        
         $ProductEdit = $this->ProductModel->getProductByID($id);
-        
         if ($ProductEdit === null) {
             die("Error: Product not found.");
         }
-
         $stockNew =$ProductEdit->stock+$stock;
-
         $productData = new Product(
             $id,
             '',
@@ -50,13 +38,10 @@ class AddquantityController extends BaseController
         $_SESSION['message'] = "Update successfully!";
         header("Location: /?controller=homeManager");
         exit();
-       
     }
-    
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $action = $_POST['action'] ?? null;
-
     switch ($action) {
         case 'add':
             $id=$_POST['IdProduct'];
@@ -64,8 +49,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $AddQuantityController= new AddQuantityController();
             $AddQuantityController->AddQuantity($id,$stock);
             exit();
-
-            
         default:
             echo "Hành động không hợp lệ!";
             break;
