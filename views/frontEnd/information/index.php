b185b6fcbd6babf851b734b1ce0ae74ce1bbf010
T14 Gen 3 <nguyennrdz@gmail.com>
2025-02-20 22:04:26 +0700

    90%-doan-20/22

--- views/frontEnd/information/index.php
+++ views/frontEnd/information/index.php
@@ -14,10 +14,8 @@ $controller->index();
 <body class="bg-gray-700 text-white">
 
 <div class="container mx-auto p-5">
-    <div class="flex flex-wrap">
-        <!-- Thông tin khách hàng -->
-        <div class="w-full md:w-1/2 p-2">
-            <div class="bg-gray-600 rounded-lg shadow-md py-6 px-14">
+    <div class="w-full ">
+            <div class="bg-gray-600 rounded-lg shadow-md py-6 px-2">
                 <h2 class="text-xl font-semibold mb-2 text-center font-bold">Customer Information</h2>
                 <div class="w-full flex justify-center">
 
@@ -34,37 +32,43 @@ $controller->index();
                 </div>
         <form action="?controller=information" method="POST" class="space-y-4">
           <input type="hidden" name="action" value="change">
-                <p class="flex items-center ">
+            <div class="w-full grid md:grid-cols-2">        
+                <p class="flex items-center  ">
                     <strong class="mr-2 ">Full Name:</strong>
                     <input type="text" name="fullName" id="fullName" class=" w-auto p-1 border-0 focus:outline-none h-8 bg-gray-600 hover:bg-green-300 rounded-2xl" placeholder="<?php echo $dataUser->FullName;?>" required>
                 </p>
                 
-                <p class="flex items-center">
-                    <strong class="mr-2">Number Phone:</strong>
-                    <input type="number" name="phone" id="phone" class="w-auto p-1 border-0 focus:outline-none h-8 bg-gray-600 hover:bg-green-300 rounded-2xl" placeholder="<?php echo $dataUser->PhoneNumber; ?>" required>
-                </p>
-                <p class="flex items-center">
-                    <strong class="mr-2">Address:</strong>
-                    <input type="text" name="address" id="address" class="flex-1 p-1 border-0 focus:outline-none h-8 bg-gray-600 hover:bg-green-300 rounded-2xl" placeholder="<?php echo $dataUser->Address ?>" required>
-                </p>
-                <p class="flex items-center">
+                <p class="flex items-center ">
                     <strong class="mr-2">Email:</strong>
                     <input type="email" name="Email" id="Email" class="w-auto p-1 border-0 focus:outline-none h-8 bg-gray-600 hover:bg-red-400 rounded-2xl" placeholder="<?php echo $Email; ?>" required readonly>
                 </p>
-                <p class="flex items-center">
+                <p class="flex items-center ">
+                    <strong class="mr-2">Address:</strong>
+                    <input type="text" name="address" id="address" class="flex-1 p-1 border-0 focus:outline-none h-8 bg-gray-600 hover:bg-green-300 rounded-2xl" placeholder="<?php echo $dataUser->Address ?>" required>
+                </p>
+                <p class="flex items-center ">
                     <strong class="mr-2">Loyalty Points:</strong>
                     <input type="text" name="LoyaltyPoints" id="LoyaltyPoints" class="w-auto p-1 border-0 focus:outline-none h-8 bg-gray-600 hover:bg-red-400 rounded-2xl" placeholder="<?php echo number_format($dataUser->LoyaltyPoints, 0, ',', '.'); ?>đ" required readonly>
                 </p>
+              
+                <p class="flex items-center ">
+                    <strong class="mr-2">Number Phone:</strong>
+                    <input type="number" name="phone" id="phone" class="w-auto p-1 border-0 focus:outline-none h-8 bg-gray-600 hover:bg-green-300 rounded-2xl" placeholder="<?php echo $dataUser->PhoneNumber; ?>" required>
+                </p>
+                
+            </div>
                 
                 <button class="w-full p-2 my-2 text-center font-bold bg-blue-500 rounded-2xl hover:bg-blue-600">
                 Change
                 </button>
         </form>                        
             </div>
-        </div>
+    </div>
 
-        <!-- Thông tin đơn hàng -->
-        <div class="w-full md:w-1/2 p-4 bg-gray-600 mt-2 rounded-lg">
+    <div class="grid md:grid-cols-2">
+
+        <!-- Thông tin đơn hàng hang cho-->
+        <div class="w-full md: p-4 bg-gray-600 mt-2 rounded-lg">
         <h2 class="text-xl font-semibold mb-2 font-bold text-center">Shopping Information</h2>
        
             <?php foreach ($dataPament as $payment): ?>
@@ -119,6 +123,60 @@ $controller->index();
                             onclick="window.location='?controller=Information&action=CancalOder&ID=<?php echo $payment['invoice']->invoiceID; ?>'"
                             class="p-2 w-full rounded bg-red-500 hover:bg-red-600 my-2 font-bold">Cancel Order</button>
                         <?php endif; ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <!-- Thông tin đơn hàng hang da dat-->
+        <div class="w-full md: p-4 bg-gray-600 mt-2 rounded-lg">
+        <h2 class="text-xl font-semibold mb-2 font-bold text-center">Shopping Information</h2>
+       
+            <?php foreach ($dataWasPayment as $payment): ?>
+                <div class="p-3 rounded bg-gray-800 m-2 hover:bg-gray-700">
+                    <div class="font-bold text-center my-2">Shopping Information ID: <?php echo $payment['invoice']->invoiceID; ?></div>
+                    <?php foreach ($payment['products'] as $productDetail): ?>
+                        <div class="bg-gray-500 rounded-lg shadow-md p-6 mb-2 flex justify-between">
+                            <div>
+                                <p><strong>Name Product:</strong> <?php echo $productDetail['product']->productName; ?> </p>
+                                <p><strong>Quantity:</strong> <?php echo $productDetail['quantity']; ?></p>
+                                <p><strong>Price:</strong> <?php echo number_format($productDetail['product']->price*$productDetail['quantity'], 0, ',', '.'); ?></p>
+                
+                            </div>
+                            <div>
+                                <img class="h-32" src="<?php echo $productDetail['product']->img; ?>">
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                    <div class="p-4 bg-gray-500 rounded">
+                        <p class=""><strong>Status: </strong><?php echo $payment['status']; ?> </p>
+                        <p class=""><strong>Number Phone: </strong><?php echo $payment['invoice']->NumberPhone; ?> </p>
+                        <p class=""><strong>Address: </strong><?php echo $payment['invoice']->Address; ?> </p>
+                        <div class="flex justify-between ">
+                            <p><strong>Date: </strong><?php echo $payment['invoice']->invoiceDate; ?></p>
+                            <p><strong>End price: </strong><?php echo $payment['invoice']->totalAmount; ?></p>
+                        </div>
+                        <p><strong class="text-green-500">Estimated delivery date: </strong><?php echo $payment['invoice']->DateDelivery; ?></p>
+                        <p><strong class="text-green-500">Note: </strong><?php echo $payment['invoice']->Note; ?></p>
+                        <?php if ($payment['status']=='delivered'): ?>
+                            <form action="?controller=information" method="POST" class="space-y-4">
+                                <input type="hidden" name="action" value="ChangeStatus">
+                                <input type="hidden" name="IdOder" value=" <?php echo $payment['invoice']->invoiceID; ?>">
+                                <input type="hidden" name="TotalAmount" value="<?php echo $payment['invoice']->totalAmount; ?>">
+                                
+                                <button class="p-2 w-full rounded bg-green-500 hover:bg-green-600 my-2 font-bold">Order Confirmation</button>
+                            </form>
+                            
+
+                        <?php endif?>
+                        
+                        
+                            <form action="?controller=information" method="POST">
+                                <input type="hidden" name="action" value="Reorder">
+                                <input type="hidden" name="InvoiceID" value="<?php echo $payment['invoice']->invoiceID; ?>">
+                                <button type="submit" class=" p-2 w-full rounded bg-purple-600 hover:bg-purple-400   my-2 font-bold">Reorder</button>
+                            </form>
+                    
+
                     </div>
                 </div>
             <?php endforeach; ?>
