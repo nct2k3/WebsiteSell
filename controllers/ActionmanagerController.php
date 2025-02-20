4b7ecdaaff52395cbe097a1782050ce1eca3640b
T14 Gen 3 <nguyennrdz@gmail.com>
2025-02-20 23:11:27 +0700

    1/20/2

--- controllers/ActionmanagerController.php
+++ controllers/ActionmanagerController.php
@@ -1,16 +1,11 @@
 <?php
-
 class ActionManagerController extends BaseController {
- 
+
     private $LoginManagerModel;
-
     public function __construct()
-    {
-        
+    { 
         $this->LoginManagerModel = $this->loadModel("LoginManagerModels");
-       
     }
-
     public function index() {
         $data = $this->LoginManagerModel-> getLoginManagerAll();
         $this->view('manager.ActionManager.index', [
